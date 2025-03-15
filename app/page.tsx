47f0b18d299125be201741de51c663af43541de8
江夏尧 <3446798488@qq.com>
2025-03-15 22:52:08 +0800

    feat: 更新服务端能力

--- app/page.tsx
+++ app/page.tsx
@@ -86,13 +86,10 @@ export default function Home() {
                 onReset={() => setChatKey((k) => k + 1)}
               />
             )}
-            instructions={`你是一个工作助手，你需要根据用户提出的工作，执行一下步骤：
-1. 提出计划分步骤列表 Markdown 格式
-2. 执行每个步骤
-3. 完成任务并一句话评估成果`}
+            instructions={`你是一个工作助手，你需要根据用户提出的工作，使用工具完成帮助用户的任务，尽量列举所有的数据，使用整洁美观的 markdown 语法`}
             labels={{
-              title: "MCP Assistant",
-              initial: "Need any help?",
+              title: "MCP 助手",
+              initial: "需要什么帮助吗？",
             }}
           />
         </div>
--- packages/server/src/crawlAgent.ts
+++ packages/server/src/crawlAgent.ts
@@ -3,7 +3,7 @@ import z from "zod";
 
 const server = new McpServer(
   {
-    name: "网络八爪鱼",
+    name: "网络助手",
     version: "1.0.0",
   },
   {
@@ -14,7 +14,7 @@ const server = new McpServer(
 );
 server.tool(
   "search_website",
-  "全网搜索数据",
+  "使用搜索引擎搜索网站",
   {
     query: z.string(),
     freshness: z
@@ -52,4 +52,76 @@ server.tool(
     };
   }
 );
+
+server.tool(
+  "search_coding_hot_articles",
+  "获取编程相关的热门文章",
+  async (args) => {
+    const res = await fetch(
+      `https://api.juejin.cn/content_api/v1/content/article_rank?category_id=1&type=hot`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+    return {
+      content: [
+        {
+          type: "text",
+          text: data.data
+            .map(
+              (item: any, index: number) =>
+                `${index + 1}. [${item.content.title}](https://juejin.cn/post/${
+                  item.content.content_id
+                })\n作者ID: ${item.content.author_id}`
+            )
+            .join("\n"),
+        },
+      ],
+    };
+  }
+);
+
+server.tool("get_recommended_articles", "获取编程推荐文章", async (args) => {
+  const res = await fetch(
+    "https://api.juejin.cn/recommend_api/v1/article/recommend_all_feed",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+      },
+      body: JSON.stringify({
+        id_type: 2,
+      }),
+    }
+  );
+  const data = await res.json();
+  return {
+    content: [
+      {
+        type: "text",
+        text: data.data
+          .map(
+            (item: any, index: number) =>
+              `${index + 1}. [${
+                item.item_info.article_info.title
+              }](https://juejin.cn/post/${item.item_info.article_id})
+
+${item.item_info.article_info.brief_content}
+
+点赞数: ${item.item_info.article_info.digg_count}
+                
+标签：${item.item_info.tags.map((i: any) => i.tag_name).join(",")}`
+          )
+          .join("\n\n"),
+      },
+    ],
+  };
+});
+
 export default server;
